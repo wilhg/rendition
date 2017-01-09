cda0b9209da23e64999c2f141ee2f9155dde23b3
cuebyte <cuebit.w@gmail.com>
2017-01-09 22:33:55 +0800

    writing spec

--- src/test/kotlin/moe/cuebyte/rendition/ModelSpec.kt
+++ src/test/kotlin/moe/cuebyte/rendition/ModelSpec.kt
@@ -1,7 +1,24 @@
 package moe.cuebyte.rendition
 
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.on
 
-object ModelSpec:Spek({
+object ModelSpec : Spek({
+  describe("id init") {
+    on("string id") {}
+    on("automated") {}
+    on("number id") {}
+  }
 
+  describe("index init") {
+    on("string index") {}
+    on("number index") {}
+  }
+
+  describe("data init") {}
+
+  describe("insert") {}
+
+  describe("batch insert") {}
 })
---
+++ src/test/kotlin/moe/cuebyte/rendition/UpdateDataSpec.kt
@@ -0,0 +1,7 @@
+package moe.cuebyte.rendition
+
+import org.jetbrains.spek.api.Spek
+
+object UpdateDataSpec : Spek({
+
+})
