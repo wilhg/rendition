9665494663849d8a785f3e46fbe7c6cf1ef6791a
cuebyte <cuebit.w@gmail.com>
2017-01-11 02:03:16 +0800

    added kotlin-test

--- src/test/kotlin/moe/cuebyte/rendition/ModelSpec.kt
+++ src/test/kotlin/moe/cuebyte/rendition/ModelSpec.kt
@@ -1,28 +1,22 @@
 package moe.cuebyte.rendition
 
-import moe.cuebyte.rendition.type.long
 import moe.cuebyte.rendition.type.string
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 object StringID : Model("Book", {
   it["id"] = string().primaryKey().auto()
 })
 
 object ModelSpec : Spek({
-  describe("init succeed") {
-    it("") {
-      assert(StringID.columns.isNotEmpty())
-    }
-  }
   describe("id init") {
-    on("string id") {
-      it("") {
-        assert(StringID.pk.info == Column.Info.STRING_PK)
-        assert(StringID.pk.automated)
-      }
+    it("string id") {
+      assertEquals(StringID.pk.info, Column.Info.STRING_PK)
+      assertTrue(StringID.pk.automated)
     }
     on("automated") {}
     on("number id") {}
