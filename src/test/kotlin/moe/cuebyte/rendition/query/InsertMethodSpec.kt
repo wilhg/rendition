5ea1b514d0b1685ccc24d81061a4f323110d100f
cuebyte <cuebit.w@gmail.com>
2017-01-12 19:46:58 +0800

    Wrote BatchInsertMethodSpec

--- src/test/kotlin/moe/cuebyte/rendition/query/InsertMethodSpec.kt
+++ src/test/kotlin/moe/cuebyte/rendition/query/InsertMethodSpec.kt
@@ -7,6 +7,7 @@ import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import redis.clients.jedis.Jedis
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 object InsertMethodSpec :Spek({
   describe("insert method") {
@@ -19,6 +20,20 @@ object InsertMethodSpec :Spek({
         it["name"] = "A"
         it["amount"] = 100
       }, "a")
+    }
+  }
+
+  describe("batch insert method") {
+    beforeGroup {
+      Connection.set(Jedis("localhost"))
+    }
+    it("should return true") {
+      assertTrue {
+        PostStr.batchInsert(listOf(
+            mapOf("id" to "123", "name" to "A", "amount" to 100),
+            mapOf("id" to "124", "name" to "A", "amount" to 100)
+        ))
+      }
     }
   }
 })
