4be4aac03352c521aecc6ad32983aafdb8d0b69e
cuebyte <cuebit.w@gmail.com>
2017-01-21 01:02:53 +0800

    little modification

--- src/main/kotlin/moe/cuebyte/rendition/Result.kt
+++ src/main/kotlin/moe/cuebyte/rendition/Result.kt
@@ -16,10 +16,10 @@ class Result(val model: Model, private val resp: Response<Map<String, String>>)
   override fun get(key: String) = lazyValue[key]
   override fun isEmpty() = lazyValue.isEmpty()
 
-  fun getId(): String = lazyValue[pkName] as String
+  internal val id: String get() = lazyValue[pkName].toString()
 
-  private val pkName = model.pk.name
-  private val lazyValue by lazy { init() }
+  private val pkName: String = model.pk.name
+  private val lazyValue: Map<String, Any> by lazy { init() }
 
   private fun init(): Map<String, Any> {
     val data = resp.get()
@@ -51,16 +51,16 @@ class ResultSet : HashSet<Result> {
 
   internal fun intersect(resultSet: ResultSet): ResultSet {
     val (bigger, smaller) = getPair(this, resultSet)
-    val set = TreeSet(bigger.map(Result::getId))
-    set.retainAll(smaller.map(Result::getId))
-    return ResultSet(model, this.filter { it.getId() in set })
+    val set = TreeSet(bigger.map(Result::id))
+    set.retainAll(smaller.map(Result::id))
+    return ResultSet(model, this.filter { it.id in set })
   }
 
   internal fun union(resultSet: ResultSet): ResultSet {
     val (bigger, smaller) = getPair(this, resultSet)
-    val baseIds = bigger.map(Result::getId).toSortedSet()
+    val baseIds = bigger.map(Result::id).toSortedSet()
     smaller
-        .filter { it.getId() !in baseIds }
+        .filter { it.id !in baseIds }
         .forEach { bigger.add(it) }
     return bigger
   }
